feb02bd406c289fd159190ae13365ab9bb6323f6
llokki <llokki92@gmail.com>
2019-12-13 06:02:15 +0300

    Скопипастил 4 урок

---
+++ J1_Stream191202/src/ru/geekbrains/java_one/lesson_d/online/Main.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.java_one.lesson_d.online;
+
+public class Main {
+
+    private static class Dog {
+        String color;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        Cat c0 = new Cat("Barsik", "Black");
+        Cat c1 = new Cat("Murzik", "White", 1);
+        Dog d0 = new Dog();
+        d0.color = c0.getColor() + " and blue";
+        System.out.println(d0.color);
+//        Cat c2 = c0;
+//        c2.age = 5;
+//
+//        System.out.println(c0.name + " " + c0.color + " " + c0.age);
+        Cat.voice();
+        System.out.println("All cats has " + Cat.pawsAmount + " paws");
+
+        c0.jump();
+        c1.jump();
+    }
+}
+}
