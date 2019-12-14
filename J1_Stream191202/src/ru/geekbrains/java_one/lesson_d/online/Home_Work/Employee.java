269af430551bdc7057fd2ed73b6968cfe11274c0
llokki <llokki92@gmail.com>
2019-12-14 16:03:40 +0300

    Ну ка

--- J1_Stream191202/src/ru/geekbrains/java_one/lesson_d/online/Home_Work/Employee.java
+++ J1_Stream191202/src/ru/geekbrains/java_one/lesson_d/online/Home_Work/Employee.java
@@ -1,4 +1,31 @@
 package ru.geekbrains.java_one.lesson_d.online.Home_Work;
 
 public class Employee {
+    private static String name;
+    private static String position;
+    private int number;
+    private int salary;
+    private static int age;
+
+    public Employee (String name,String position,int number,int salary,int age) {
+        this.age = age;
+        this.name = name;
+        this.number = number;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getName() {return name;}
+    public String getPosition() {return position;}
+    public int getNumber() {return number;}
+    public int getSalary() {return salary;}
+    public static int getAge() {return age;}
+
+
+    public static void salaryUp() {
+            if (getAge() > 40) {
+                System.out.println(name + " , " + position);
+            }
+        }
+
 }
--- J1_Stream191202/src/ru/geekbrains/java_one/lesson_d/online/Home_Work/Main.java
+++ J1_Stream191202/src/ru/geekbrains/java_one/lesson_d/online/Home_Work/Main.java
@@ -1,4 +1,16 @@
 package ru.geekbrains.java_one.lesson_d.online.Home_Work;
 
+
 public class Main {
+    public static void main(String[] args){
+
+        Employee x1 = new Employee("Petrov Petr Petrovich", "Emperor of the Mankind", 8883974, 12000, 32);
+        Employee x2 = new Employee("Van Hellsing", "painter", 1361355, 333000, 46);
+        Employee x3 = new Employee("Gogi Volosatie Nogi", "warrior", 5432452, 125400, 34);
+        Employee x4 = new Employee("Ceritelli Givi Zurabovich", "writer", 3451255, 122, 57);
+        Employee x5 = new Employee("Agtonom Agronomov", "butcher", 4556266, 4000, 98);
+
+        Employee.salaryUp();
+
+    }
 }
