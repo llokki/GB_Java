0582e0ba310570f749ca9f21cdb03e2216c00bfe
llokki <llokki92@gmail.com>
2020-03-01 14:26:35 +0300

    Прошел 4 урок

--- J1_Stream191202/src/java_one_V2/lesson_a/online/HelloWorld.java
+++ J1_Stream191202/src/java_one_V2/lesson_a/online/HelloWorld.java
@@ -1,4 +1,4 @@
-package java_one_V2.lessonn_a.online;
+package java_one_V2.lesson_a.online;
 
 public class HelloWorld {
 
---
+++ J1_Stream191202/src/java_one_V2/lesson_d/online/zoo/Cat.java
@@ -0,0 +1,55 @@
+package java_one_V2.lesson_d.online.zoo;
+
+public class Cat {
+    private String name;
+    private String color;
+    private int birthYear;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setName(String name) {
+        this.name  = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return 2019 - birthYear;
+    }
+
+    private void setAge(int age) {
+        this.birthYear = 2019 - age;
+    }
+
+    public static final int pawsAmount = 4;
+    private static double stomachVolume = 100;
+
+    public Cat() { setAge(0);}
+
+    public Cat(String name, String color) {
+        this();
+        this.name = name;
+        this.color = color;
+        this.voice();
+    }
+
+    public Cat(String name, String color, int birthYear) {
+        this.name = name;
+        this.color = color;
+        this.birthYear = birthYear;
+    }
+
+    public static void voice() {
+        System.out.println("Meows!");
+    }
+
+    public void jump() {
+        if (getAge() < 5) {
+            System.out.println(name + " jumps!");
+        }
+    }
+}
