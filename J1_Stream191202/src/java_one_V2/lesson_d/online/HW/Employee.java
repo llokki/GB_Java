05dcde55587798ae48a2552e88bbead5ece594ea
llokki <llokki92@gmail.com>
2020-03-03 06:08:49 +0300

    Дз по 4 занятию

--- J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Employee.java
+++ J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Employee.java
@@ -1,9 +1,12 @@
 package java_one_V2.lesson_d.online.HW;
 
+import java.util.Arrays;
+
 public class Employee {
     private String name;
     private String position;
     private int birthYear;
+    private int salary;
 
     public void setName(String name) { this.name = name; }
 
@@ -19,10 +22,26 @@ public class Employee {
         this.birthYear = 2020 - age;
     }
 
+    public void setSalary (int salary) {this.salary = salary;}
 
-    public Employee (String name, String position, int birthYear) {
+    public int getSalary() { return salary;}
+
+    public Employee (String name, String position, int birthYear, int salary) {
         this.name = name;
         this.position = position;
         this.birthYear = birthYear;
+        this.salary = salary;
+    }
+
+    public void printer() {
+        if( getAge() > 40){
+            System.out.println("Name: " + getName() + " Position: " + getPosition() );
+        }
+    }
+    public void upper() {
+        if ( getAge() > 45) {
+            salary = salary + 5000;
+            System.out.println(getName() + " старше 45 лет и его зарплата теперь " + getSalary() + "рублёв");
+        }
     }
 }
--- J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Main.java
+++ J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Main.java
@@ -1,8 +1,20 @@
 package java_one_V2.lesson_d.online.HW;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main (String[] args){
 
+        Employee [] arr = new Employee[5];
 
+        arr[0] = new Employee("Darius", "elektrik", 1923, 4245123);
+        arr[1] = new Employee("Malkolm", "mechanicum", 1023,1234135);
+        arr[2] = new Employee("Leonidas", "astartes", 1567,234234234);
+        arr[3] = new Employee("Gunnar", "predator", 1000,2346543);
+        arr[4] = new Employee("Kroager", "alien", 1234,344456456);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].printer();
+        }
     }
 }
