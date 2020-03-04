2b35f65ae089edbf160f51cbe54aae7229865e56
llokki <llokki92@gmail.com>
2020-03-04 21:17:56 +0300

    Доделал дз к 4 уроку, самостоятельно!

--- J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Employee.java
+++ J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Employee.java
@@ -1,12 +1,15 @@
 package java_one_V2.lesson_d.online.HW;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Employee {
+    private Random random = new Random();
     private String name;
     private String position;
     private int birthYear;
     private int salary;
+    private int id;
 
     public void setName(String name) { this.name = name; }
 
@@ -26,7 +29,11 @@ public class Employee {
 
     public int getSalary() { return salary;}
 
+    public int getId() {return id = random.nextInt(10);}
+
+    public Employee (){getId();}
     public Employee (String name, String position, int birthYear, int salary) {
+        this();
         this.name = name;
         this.position = position;
         this.birthYear = birthYear;
@@ -35,13 +42,13 @@ public class Employee {
 
     public void printer() {
         if( getAge() > 40){
-            System.out.println("Name: " + getName() + " Position: " + getPosition() );
+            System.out.println("Name: " + getName() + " Position: " + getPosition() + " " + getAge() + " " + getId());
         }
-    }
+    } // выводим даные о сотрудниках старше 40 лет + идентификатор
     public void upper() {
         if ( getAge() > 45) {
             salary = salary + 5000;
-            System.out.println(getName() + " старше 45 лет и его зарплата теперь " + getSalary() + "рублёв");
+            System.out.println(getName() + " старше 45 лет и его зарплата теперь " + getSalary() + " рублёв");
         }
-    }
+    } // повышаем на 5к сотрудникам старше 45 лет
 }
--- J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Main.java
+++ J1_Stream191202/src/java_one_V2/lesson_d/online/HW/Main.java
@@ -7,14 +7,17 @@ public class Main {
 
         Employee [] arr = new Employee[5];
 
-        arr[0] = new Employee("Darius", "elektrik", 1923, 4245123);
-        arr[1] = new Employee("Malkolm", "mechanicum", 1023,1234135);
-        arr[2] = new Employee("Leonidas", "astartes", 1567,234234234);
-        arr[3] = new Employee("Gunnar", "predator", 1000,2346543);
-        arr[4] = new Employee("Kroager", "alien", 1234,344456456);
+        arr[0] = new Employee("Darius", "elektrik", 1999, 4245123);
+        arr[1] = new Employee("Malkolm", "mechanicum", 1992,1234135);
+        arr[2] = new Employee("Leonidas", "astartes", 1945,234234234);
+        arr[3] = new Employee("Gunnar", "predator", 1934,2346543);
+        arr[4] = new Employee("Kroager", "alien", 1965,344456456);
 
         for (int i = 0; i < arr.length; i++) {
             arr[i].printer();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].upper();
         }
     }
 }
