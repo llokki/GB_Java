85606c1cfd17a4caa7c7e8c76457a561403b8f86
llokki <llokki92@gmail.com>
2020-05-24 17:50:18 +0300

    Доделал ДЗ ко 2 уроку

--- J1_Stream191202/src/ru/geekbrains/java_two/lesson2/hw/Main.java
+++ J1_Stream191202/src/ru/geekbrains/java_two/lesson2/hw/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String [][] arr = {
                 {"24", "7", "4545","123"},
                 {"8" ,"45","2","13"},
-                {"565" , "4534", "32", "34"},
+                {"565" , "4534", "а2", "34"},
                 {"123", "313", "344", "5636"}
         };
         try {
@@ -40,7 +40,7 @@ public class Main {
                     arr1[i][j] = Integer.parseInt(arr [i][j]);
                     sum += arr1[i][j];
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("format " + i + " " + j, i,j);
+                    throw new MyArrayDataException("error is here: " + i + " " + j, i,j);
                 }
             }
         }
