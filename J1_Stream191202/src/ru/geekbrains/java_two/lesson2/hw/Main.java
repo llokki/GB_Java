7069c3f6c63cfdad8130df69398d941ae88e20cc
llokki <llokki92@gmail.com>
2020-05-24 17:39:35 +0300

    Доделал домашнее задание ко второму уроку

--- J1_Stream191202/src/ru/geekbrains/java_two/lesson2/hw/Main.java
+++ J1_Stream191202/src/ru/geekbrains/java_two/lesson2/hw/Main.java
@@ -2,8 +2,7 @@ package ru.geekbrains.java_two.lesson2.hw;
 
 public class Main {
 
-
-
+    static final int array_size = 4;
 
     public static void main(String[] args)  {
         String [][] arr = {
@@ -12,30 +11,39 @@ public class Main {
                 {"565" , "4534", "32", "34"},
                 {"123", "313", "344", "5636"}
         };
-        System.out.println(ArrayReBuild(arr));
-
-        //System.out.println(array.length);
-//        System.out.println(array[2].length);
-
-
+        try {
+            System.out.println(ArrayReBuild(arr));
+        } catch (MyArraySizeException e) {
+           e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+            System.out.println("Неправильный элемент массива: " + e.getI() + ", " + e.getJ());
+        }
     }
 
-    public static int ArrayReBuild (String [][] arr ) throws MyArrayDataException {
+    public static int ArrayReBuild (String [][] arr ) throws MyArraySizeException, MyArrayDataException {
+        if (arr.length !=array_size) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != array_size) {
+                throw new MyArraySizeException();
+            }
+        }
+
         int sum = 0;
 
         int [][] arr1 = new int[4][4];
         for (int j = 0; j < arr1.length; j++) {
             for (int i = 0; i < arr1.length; i++) {
-                arr1[i][j] = Integer.parseInt(arr [i][j]);
-
-
-
-                sum += arr1[i][j];
+                try {
+                    arr1[i][j] = Integer.parseInt(arr [i][j]);
+                    sum += arr1[i][j];
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("format " + i + " " + j, i,j);
+                }
             }
         }
-
         return sum;
     }
-
-
 }
--- J1_Stream191202/src/ru/geekbrains/java_two/lesson2/hw/MyArraySizeException.java
+++ J1_Stream191202/src/ru/geekbrains/java_two/lesson2/hw/MyArraySizeException.java
@@ -3,7 +3,4 @@ package ru.geekbrains.java_two.lesson2.hw;
  *Данное исключение выводит предупреждение, если длина массива не является верной
  * */
 public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
 }
